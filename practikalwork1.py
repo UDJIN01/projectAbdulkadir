37adedb2a9c2556797655ee131658889b4e99b7d
a1 <dadadaret5@mail.ru>
2023-08-09 21:52:07 +0300

    first

---
+++ practikalwork1.py
@@ -0,0 +1,23 @@
+a = 5
+b = 2
+print(a + b) # Сложение
+print(a - b) # Вычитание
+print(a * b) # Умножение
+print(a / b) # Деление
+
+
+print()
+
+
+print(a > b) # больше
+print(a < b) # меньше
+print(a == b) # равно
+print(a != b) # не равно
+
+
+print()
+
+
+print("Hello, world! My name Abdul Magomedaliew") # вывод строки
+x = input("Введите число: ") # ввод данных
+print("Вы ввели число " + x) # вывод строки с переменной
